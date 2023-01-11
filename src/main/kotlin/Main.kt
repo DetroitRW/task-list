9c1050c6a2bff8a89283a1978507a75679c51de6
Ilya Brant <detroitib31@gmail.com>
2023-01-11 16:54:10 +0600

    Написал алгорим для выполнения действий

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,6 +10,50 @@ fun main(args: Array<String>) {
         val command = scanner()
 
 
+
+        when(command) {
+            "+" -> {
+                print("Добавьте задачу: ")
+                val task2 = scanner()
+                newTasks.addTask(task2)
+            }
+            "=" -> {
+                for (i in 0 .. newTasks.getValue().size -1) {
+                    print("Невыполненные задачи: $i")
+                    print(": ")
+                    println(newTasks.getValue()[i])
+                }
+                for (i in 0 .. doneTasks.getValue().size -1) {
+                    print("Выполненные задачи: $i")
+                    print(": ")
+                    println(doneTasks.getValue()[i])
+                }
+
+            }
+            "-" -> {
+                print("Какую задачу удалить?: ")
+                val index = Scanner(System.`in`).nextInt()
+                newTasks.remove(index)
+            }
+            "^" -> {
+                print("Какую задачу отметить как выполненую?: ")
+                val index = Scanner(System.`in`).nextInt()
+                val task = newTasks.getValue().get(index)
+                newTasks.remove(index)
+                doneTasks.addTask(task)
+
+            }
+            "*" -> {
+                print("С какой задачи снять отметку?: ")
+                val index = Scanner(System.`in`).nextInt()
+                val task = doneTasks.getValue().get(index)
+                doneTasks.remove(index)
+                newTasks.addTask(task)
+            }
+        }
+    }
+}
+
 fun menu() {
     println("Возможные команды:")
     println("- Отобразить список задач (=)")
